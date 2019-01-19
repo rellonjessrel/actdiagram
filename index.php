3c8b58d19bc7c02acb89b53a5088a12847089e24
rellonjessrel <35474756+rellonjessrel@users.noreply.github.com>
2019-01-20 02:29:46 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>class diagram</title>
+</head>
+<body>
+<img src="act.jpg">
+<p> This an activity diagram shows the relative to each other </p>
+<p> if where we can clearly see the difference the data flow of a program the managing of the activity</p>
+<p> so that they well be guided accordingly according to thier table also</p>
+<p> to avoid chaos in the  system developer must set an edge to every user to set  thier limit toaccess the application</p>
+<p> to manage the application the developer must know this kind of diagram</p>
+</body>
+</html>
